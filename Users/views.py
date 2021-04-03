05e106dea85ce68279122cb5283133e24fe5dc92
shaktirajput100 <shakti@availfinance.in>
2021-04-03 13:28:03 +0530

    Users app created

---
+++ Users/views.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import messages
+from django.shortcuts import render, redirect
+
+# Create your views here.
+from forms import UserRegisterForm
+
+
