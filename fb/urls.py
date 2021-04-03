5e41c35d2c4ab5bfec6e8270c487deac55c5748e
shaktirajput100 <shakti@availfinance.in>
2021-04-03 19:44:28 +0530

    login page added on localhost/login link

--- fb/urls.py
+++ fb/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from Users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('register/', user_views.register, name='register'),
+    url('login/', auth_views.LoginView.as_view(template_name='Users/login.html'), name='login'),
     url('', include('blog.urls')),
 ]
