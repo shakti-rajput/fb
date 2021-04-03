dbcec4cd53dcc64ea1452eac273aa6b4cb8aa3d0
shaktirajput100 <shakti@availfinance.in>
2021-04-03 15:58:21 +0530

    Post request after submitting form is working now

--- Users/views.py
+++ Users/views.py
@@ -2,12 +2,21 @@
 from __future__ import unicode_literals
 
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
 
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            str1 = 'Account created for ' + {username} + '!'
+            messages.success(request, str1)
+            return redirect('blog-home')
+    else:
+        form = UserCreationForm()
     return render(request, 'Users/register.html', {'form': form})
