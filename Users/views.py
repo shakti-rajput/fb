a28bb184a2448e9011d4b398023347daac3df08c
shaktirajput100 <shakti@availfinance.in>
2021-04-03 16:44:39 +0530

    Email field is added in UserCreation Forms

--- Users/views.py
+++ Users/views.py
@@ -1,22 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
-
+from .forms import UserRegisterForm
 # Create your views here.
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            str1 = 'Account created for ' + {username} + '!'
+            str1 = 'Account created for ' + str({username}) + '!'
             messages.success(request, str1)
             return redirect('blog-home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'Users/register.html', {'form': form})
