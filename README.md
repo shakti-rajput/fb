83b9196257ab799622e41b0e2b7cfb65d0a5b5a6
shaktirajput100 <shakti@availfinance.in>
2021-04-03 12:37:11 +0530

    Created a model and registered it to view in admin

--- README.md
+++ README.md
@@ -10,3 +10,16 @@
 *python manage.py makemigrations*
 
 *python manage.py startapp blog*
+
+*python manage.py createsuperuser*
+
+*python manage.py sqlmigrate blog 0001*
+
+<mark>author = models.ForeignKey(User, on_delete=models.CASCADE)</mark>
+post.author.email
+
+user.post_set
+
+user.post_set.all()
+
+user.post_set.create(title='Blog 3', content='Third Post Content!')
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.utils import timezone
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
