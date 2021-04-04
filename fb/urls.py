96b5b878d307628cf6262f35162c9eae3ee8358d
shaktirajput100 <shakti@availfinance.in>
2021-04-04 08:11:31 +0530

    For Profile media directory was made

--- fb/urls.py
+++ fb/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from Users import views as user_views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -26,3 +28,6 @@ urlpatterns = [
     url('profile/', user_views.profile, name='profile'),
     url('', include('blog.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
