c426d05ae2de1008ee5a9bec0617d2b12b2dbb2f
shaktirajput100 <shakti@availfinance.in>
2021-04-04 09:31:08 +0530

    Profile picture save function overridden to decrease the size of profile pics

--- Users/models.py
+++ Users/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.contrib.auth.models import User
-
+from PIL import Image
 from django.db import models
 
 
@@ -13,3 +13,12 @@ class Profile(models.Model):
     def __str__(self):
         str1 = str({self.user.username}) + 'Profile'
         return str1
+
+    def save(self):
+        super(Profile, self).save()
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
