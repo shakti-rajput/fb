f6df044fac4e14a3970738f5e0d01047f92e92b1
shaktirajput100 <shakti@availfinance.in>
2021-04-04 07:48:59 +0530

    Profile model registered

--- Users/models.py
+++ Users/models.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.contrib.auth.models import User
 
 from django.db import models
 
+
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        str1 = str({self.user.username}) + 'Profile'
+        return str1
